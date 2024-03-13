eeb686fee2ea3c47d824cfd62b08005357758008
Styd051 <thierus.dylan@gmail.com>
2024-03-13 19:46:27 +0100

    fixes for arch and manjaro builds by Gibbon

--- src/engine/p_map.c
+++ src/engine/p_map.c
@@ -1014,9 +1014,9 @@ extern fixed_t  topslope;
 extern fixed_t  bottomslope;
 
 // [kex]
-fixed_t laserhit_x;
-fixed_t laserhit_y;
-fixed_t laserhit_z;
+extern fixed_t laserhit_x;
+extern fixed_t laserhit_y;
+extern fixed_t laserhit_z;
 
 
 //
