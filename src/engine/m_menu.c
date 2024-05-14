7fdc8bcdd01245dcbf8222361ea89fcf151d24ce
Dylan THIERUS <thierus.dylan@gmail.com>
2024-05-14 09:22:45 +0200

    float fixing and thermo dragging fixing Fixes some more ints to floats that we need for accuracy and also fixes the incorrect logic for thermobar dragging with the mouse.
    SDL3 porting is now completed and back up to parity with the SDL2 version. by Gibbon

--- src/engine/m_menu.c
+++ src/engine/m_menu.c
@@ -3732,8 +3732,8 @@ static void M_CheckDragThermoBar(event_t* ev, menu_t* menu) {
 	menuthermobar_t* bar;
 	float startx;
 	float scrny;
-	int x;
-	int y;
+	float x;
+	float y;
 	int i;
 	float mx;
 	float my;
@@ -3749,7 +3749,7 @@ static void M_CheckDragThermoBar(event_t* ev, menu_t* menu) {
 	}
 
 	// mouse buttons must be held and moving
-	if (!(ev->data1 & 1) || (ev->data2 != 0.0 || ev->data3 != 0.0)) {
+	if (!(ev->data1 & 1)) {
 		return;
 	}
 
@@ -3762,16 +3762,16 @@ static void M_CheckDragThermoBar(event_t* ev, menu_t* menu) {
 		((float)SCREENHEIGHT * video_ratio)) * menu->scale;
 	scaley = ((float)video_height /
 		(float)SCREENHEIGHT) * menu->scale;
-	startx = (float)x * scalex;
+	startx = x * scalex;
 	width = startx + (100.0f * scalex);
 
 	// check if cursor is within range
 	for (i = 0; bar[i].item != -1; i++) {
 		lineheight = (float)(LINEHEIGHT * bar[i].item);
-		scrny = ((float)y + lineheight + 10) * scaley;
+		scrny = (y + lineheight + 10) * scaley;
 
 		if (my < scrny) {
-			scrny = ((float)y + lineheight + 2) * scaley;
+			scrny = (y + lineheight + 2) * scaley;
 			if (my >= scrny) {
 				// dragged all the way to the left?
 				if (mx < startx) {
